5c225158400ed3f0c3a7f55e02d018e220d05fab
Aleksei Pukhovoi <Aleksei_Pukhovoi@epam.com>
2022-05-11 09:11:30 +0300

    add schema.sql and data.sql files

---
+++ src/main/resources/data.sql
@@ -0,0 +1,8 @@
+INSERT INTO regions (name, short_name)
+ VALUES
+ ('Khanty-Mansi autonomous area - Yugra', 'Ugra'),
+ ('The Saha republic', 'Yakutia'),
+ ('Chuvash Republic', 'Chuvashia'),
+ ('Pskov region', 'Pskov region'),
+ ('Tyva Republic', 'Tyva'),
+ ('Republic of Khakassia', 'Khakassia');
---
+++ src/main/resources/schema.sql
@@ -0,0 +1,5 @@
+CREATE TABLE IF NOT EXISTS `REGIONS`(
+    `id`              BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    `name`            VARCHAR(100) NOT NULL,
+    `short_name`      VARCHAR(100) NOT NULL
+);
